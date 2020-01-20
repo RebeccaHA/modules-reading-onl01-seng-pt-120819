120f2458ab56f4f17a5caa7c699381ba94386c4a
calm-block-7153 <rebeccahunt034@gmail.com>
2020-01-20 20:28:17 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,15 +3,6 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
-    extend 
-   FancyDance::ClassMethods
-   include
-   FancyDance::InstanceMehods
-  
- 
-  attr_accessor :name
- 
-  def initialize(name)
-    @name = name
-  end
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 end
